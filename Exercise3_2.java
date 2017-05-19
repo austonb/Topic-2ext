9c9a56699a6ede1eff19659136ccbf5bfec6e2bf
austonb <p2025042@mvrht.com>
2017-05-19 11:29:01 -0700

    Add files via upload

---
+++ Exercise3_2.java
@@ -0,0 +1,25 @@
+package chapter3;
+import java.util.Scanner;
+
+public class Exercise3_2 {
+	private static Scanner input;
+
+	public static void main(String[] args) {
+		input = new Scanner(System.in);
+
+		// Generate three random integers 
+		int digit1 = (int)(Math.random() * 10);
+		int digit2 = (int)(Math.random() * 10);
+		int digit3 = (int)(Math.random() * 10);
+
+		// Prompt user to enter the sum of three integers
+		System.out.print(
+			"What is " + digit1 + " + " + digit2 + " + " + digit3 + "? ");
+		int answer = input.nextInt();
+		
+		System.out.println(
+			digit1 + " + " + digit2 + " + " + digit3 + " = " + answer + " is " +
+			(digit1 + digit2 + digit3 == answer));
+	}
+}
+
